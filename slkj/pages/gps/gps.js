0c0d6e048a4cd3def886c78a4a1347fe04e382e2
maxh <maxuhui2113@163.com>
2018-02-11 09:21:27 +0800

    maxh
    gps定位修改

--- slkj/pages/gps/gps.js
+++ slkj/pages/gps/gps.js
@@ -1,52 +1,103 @@
 //index.js
 //获取应用实例 
 var app = getApp()
+var util = require('../../utils/util');
+var network_util = require('../../utils/network_util');
+var json_util = require('../../utils/json_util');
 Page({
   data: {
     carNumber: "",
-    map: {
-      lat: 0,
-      lng: 0,
-      iconPath:'',
-      markers: [],
-      hasMarkers: false//解决方案  
-    } 
+    scale: '15', //缩放
+    Height: '0',
+    controls: '40',//中心点
+    latitude: '',
+    longitude: '',
+    markers: [],
+    listData: []
   },
   onLoad: function (options) {
-    var that = this;
+    let that = this//不要漏了这句，很重要
+    this.getMapD();
     wx.getLocation({
-      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标  
-      success: function (res) {
-        // success  
-        // wx.request({
-        //   url: 'https://xxx.com/detail.htm?vid=3&latlng=' + res.latitude + ',' + res.longitude,
-        //   data: {},
-        //   method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
-        //   // header: {}, // 设置请求的 header  
-        //   success: function (res) {
-            // success  
-            that.setData({
-              'map.lat': 23.099994,
-              'map.lng': 113.324520,
-              'map.markers': [{
-                latitude: 23.099994,
-                longitude: 113.324520,
-                name: '00000',
-                desc: '123123123',
-                iconPath: '/res/images/location.png',
-              }],
-              'map.hasMarkers': true//解决方案  
-            });
-        //   }
-        // })
+      type: 'wgs84', //返回可以用于wx.openLocation的经纬度
+      success: (res) => {
+        that.setData({
+          markers: that.getSchoolMarkers(),
+          // scale: 12,
+          longitude: res.longitude,
+          latitude: res.latitude
+        })
       }
-    })
+    });
   },
+  getMapD() {
+    let that = this//不要漏了这句，很重要
+    //GET方式
+    var url1 = 'https://zhihu-daily.leanapp.cn/api/v1/last-stories';
+    network_util._get(url1, function (res) {
+      console.log(res);
+      that.setData({
+        'listData': [
+          {
+            "id": 1,
+            "placeName": "金色尚都",
+            "placeImageUrl": "",
+            "placeOpenTime": 1505854800,
+            "placeCloseTime": 1505919600,
+            "placeAddress": "河北省邯郸市丛台区金色尚都A座",
+            "placeLongitude": "114.49705",
+            "placeLatitude": "36.63881"
+          }],
+      });
+    }, function (res) {
+      console.log(res);
+    });
+  },
+  getSchoolMarkers() {
+
+    var market = [];
+
+    for (let item of this.data.listData) {
+
+      let marker1 = this.createMarker(item);
+
+      market.push(marker1)
+    }
+    return market;
+  },
+  strSub: function (a) {
+    var str = a.split(".")[1];
+    str = str.substring(0, str.length - 1)
+    return a.split(".")[0] + '.' + str;
+  },
+  createMarker(point) {
+
+    let latitude = this.strSub(point.placeLatitude);
+    let longitude = point.placeLongitude;
+    let marker = {
+      iconPath: "/res/images/location.png",
+      id: point.id || 0,
+      name: point.placeName || '',
+      title: point.placeName || '',
+      latitude: latitude,
+      longitude: longitude,
+      label: {
+        x: -24,
+        y: -26,
+        content: point.placeName
+      },
+      width: 30,
+      height: 30
+    };
+    return marker;
+  },
+  //  车牌输入框
   carNumberInput: function (e) {
     this.setData({
       carNumber: e.detail.value
     })
   },
+  // 点击查询事件
   clickButton: function (e) {
     let that = this//不要漏了这句，很重要
     wx.request({
--- slkj/pages/index/index.js
+++ slkj/pages/index/index.js
@@ -16,30 +16,8 @@ Page({
   * 首页banner
   */
   setBanner: function () {
-    // util.fetch('https://zhihu-daily.leanapp.cn/api/v1/last-stories', function (data) {
-    //   that.setData({
-    //     banner: data.STORIES.stories
-    //   });
-    // });
-
     let that = this//不要漏了这句，很重要
-    wx.request({
-      url: 'https://zhihu-daily.leanapp.cn/api/v1/last-stories',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      success: function (res) {
-        //将获取到的json数据，存在名字叫zhihu的这个数组中
-        that.setData({
-          banner: res.data.STORIES.stories,
-          //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
-
-        })
-      }
-    })
-
     //GET方式
- 
     var url1 = 'https://zhihu-daily.leanapp.cn/api/v1/last-stories';
     network_util._get(url1, function (res) {
       console.log(res);
